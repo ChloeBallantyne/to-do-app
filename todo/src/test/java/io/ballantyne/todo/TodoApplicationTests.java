50c8bd46d2613a04cb6ea3a42f7fa31957283294
ChloeBallantyne <chloeballantyne@gmail.com>
2024-05-21 19:51:50 +1000

    initial commit

---
+++ todo/src/test/java/io/ballantyne/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ballantyne.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
